83707b7e8dd250e4e341ef60709a5e28b65a9f1c
Colton M <colton.marschik@gmail.com>
2019-03-12 15:16:22 -0600

    added consumer info to .gitignore

--- constants.py
+++ constants.py
@@ -1,6 +1,3 @@
-CONSUMER_KEY =
-CONSUMER_SECRET =
-
-REQUEST_TOKEN_URL =
-ACCESS_TOKEN_URL =
-AUTHORIZATION_URL =
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
