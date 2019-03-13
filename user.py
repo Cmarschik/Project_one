ee1774efb5e256363df7fd16c547e63b1114776f
Colton M <colton.marschik@gmail.com>
2019-03-13 13:24:00 -0600

    Created user file

--- user.py
+++ user.py
@@ -2,3 +2,39 @@ from database import CursorFromConnectionPool
 import oauth2
 import json
 from twitter_utils import consumer
+
+class User:
+    def __init__(self, screen_name, oauth_token, oauth_token_secret, id):
+        self.screen_name = screen_name
+        self.oauth_token = oauth_token
+        self.oauth_token_secret = oauth_token_secret
+        self.id = None
+
+    def __repr__(self):
+        return "<User: {}".format(self.screen_name)
+
+    def save_to_db(self):
+        with CursorFromConnectionPool() as cursor:
+            cursor.execute('INSERT INTO users (screen_name, oauth_token, oauth_token_secret) VALUES(%s, %s, %s )',
+                           (self.screen_name, self.oauth_token, self.oauth_token_secret))
+
+    @classmethod
+    def load_from_db_by_screen_name(cls, screen_name):
+        with CursorFromConnectionPool() as cursor:
+            cursor.execute('SELECT * FROM users WHERE screen_name=%s', (screen_name,))
+            user_data = cursor.fetchone()
+            if user_data:
+                return cls(screen_name=user_data[1],
+                           oauth_token=user_data[2],
+                           oauth_token_secret=user_data[3],
+                           id=user_data[0])
+
+    def twitter_request(self, URL, verb='GET'):
+        authorized_token = oauth2.Token(self.oauth_token, self.oauth_token_secret)
+        authorized_client = oauth2.Client(consumer, authorized_token)
+
+        response, content = authorized_client.request(URL, verb)
+        if response.status != 200:
+            print("An error occured while searching!")
+
+        return json.loads(content.deconde('UTF-8'))
